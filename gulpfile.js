e9f948fac887be848921bd14a10e978fbfdccf10
Peter Mouland <peter.mouland@gmail.com>
2015-01-02 14:43:23 +0000

    add code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,3 @@ var gulp = require('gulp');
 var componentHelper = require('gulp-component-helper')(gulp);
 var paths = componentHelper.paths;
 var runSequence = require('run-sequence');
-
-
-var karma = require('karma').server;
-/**
- * Run test once and exit
- */
-gulp.task('test', function (done) {
-    karma.start({
-        configFile: __dirname + '/test/karma.conf.js',
-        singleRun: true
-    }, done);
-});
-
-/**
- * Watch for file changes and re-run tests on each change
- */
-gulp.task('tdd', function (done) {
-    karma.start({
-        configFile: __dirname + '/test/karma.conf.js'
-    }, done);
-});
